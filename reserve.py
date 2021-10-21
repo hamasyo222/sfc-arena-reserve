616724a3a9e0f780d23b62c6d6bac0b60367e9d5
hamasyo <hamasyo222@outlook.jp>
2021-10-22 02:34:37 +0900

    Update reserve.py

--- reserve.py
+++ reserve.py
@@ -104,7 +104,7 @@ def reserve(event):
     m = res_date.month
     d = res_date.day
 
-    while datetime.datetime.now() < datetime.datetime(y, m, d, 2, 32, 00):
+    while datetime.datetime.now() < datetime.datetime(y, m, d, 0, 0, 00):
         time.sleep(1)
 
 
