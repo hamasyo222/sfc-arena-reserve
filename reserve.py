52b3e59ac4a5140274dccd12403f659f45f49ffb
hamasyo <51374934+hamasyo222@users.noreply.github.com>
2021-11-08 04:02:43 +0900

    double 実装

--- reserve.py
+++ reserve.py
@@ -26,102 +26,6 @@ import sys
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-def calender():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    min = (datetime.datetime.utcnow() + datetime.timedelta(days=14)).isoformat() + 'Z' # 'Z' indicates UTC time
-    max = (datetime.datetime.utcnow() + datetime.timedelta(days=15)).isoformat() + 'Z'
-    print('Getting the 2weeks later event')
-    print(min)
-    print(max)
-    events_result = service.events().list(calendarId='3442e499hjv4j581l1c68n4v2g@group.calendar.google.com', timeMin=min,
-                                        timeMax=max,maxResults=1, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        reserve(event)
-        
-def enter(driver, start_hour, start_minute, end_hour, end_minute):
-
-    #開始プルダウン
-    #時間
-    Select(driver.find_element_by_css_selector('#reservation_start_h')).select_by_value(start_hour)
-    #分
-    Select(driver.find_element_by_css_selector('#reservation_start_m')).select_by_value(start_minute)
-    #終了プルダウン
-    #時間
-    Select(driver.find_element_by_css_selector('#reservation_end_h')).select_by_value(end_hour)
-    #分
-    Select(driver.find_element_by_css_selector('#reservation_end_m')).select_by_value(end_minute)
-
-    #実開始プルダウン
-    #時間
-    Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(10) > div > div:nth-child(2) > select')).select_by_value(start_hour)
-    #分
-    Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(10) > div > div:nth-child(3) > select')).select_by_value(start_minute)
-
-    #実終了プルダウン
-    #時間
-    Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(12) > div > div:nth-child(1) > select')).select_by_value(end_hour)
-    #分
-    Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(12) > div > div:nth-child(2) > select')).select_by_value(end_minute)
-
-    #名称
-    driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(18) > input[type=text]').send_keys('バドミントン練習')
-
-    #人数(塾内)
-    driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(20) > div > input:nth-child(1)').clear()
-    driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(20) > div > input:nth-child(1)').send_keys('15')
-
-    #e-mail
-    driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(30) > div > input[type=text]:nth-child(1)').send_keys('t20651sh@sfc.keio.ac.jp')
-
-    #連絡先
-    driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(32) > input[type=text]').send_keys('08014671953')
-
-    #登録する
-    driver.find_element_by_css_selector('#reservation-form > button').click()
-
-    #画面遷移
-
-    #ダイアログ (最終)
-    while not driver.find_element_by_css_selector('body > div:nth-child(20)').is_displayed():
-        print("waiting on display")
-    driver.find_element_by_css_selector('body > div:nth-child(20) > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(1)').click()
-
-    #画面遷移
-    time.sleep(2)
-
-    #完了確認
-    driver.find_element_by_css_selector('body > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog-buttons.ui-draggable.ui-resizable > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button').click()
-
 
 def reserve(event):
     try:    
@@ -178,10 +82,10 @@ def reserve(event):
         ##keio.jp認証有無(遷移確認)
         ##if "https://auth.keio.jp/idp/profile/SAML2/Redirect/SSO" in driver.current_url:
         #ID
-        driver.find_element_by_css_selector('#username').send_keys(os.environ['KEIO_ACCOUNT'])
+        driver.find_element_by_css_selector('#username').send_keys(os.environ['KEIO_ACCOUNT'])#
 
         #パスワード
-        driver.find_element_by_css_selector('#password').send_keys(os.environ['KEIO_PASSWORD'])
+        driver.find_element_by_css_selector('#password').send_keys(os.environ['KEIO_PASSWORD'])#
 
         #ログイン
         driver.find_element_by_css_selector('#login > section.form-element-wrapper.login_b > button').click()
@@ -225,20 +129,111 @@ def reserve(event):
 
         #画面遷移
 
+
         #時間選択 アリーナ手前
         driver.find_element_by_css_selector('#main_content > div.container > div.container_body.noscroll > div.fix_tbl_area.time_table.found-reservable > div.fix_bottom_right > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div > label:nth-child(61)').click()
-        enter(driver, start_hour, start_minute, end_hour, end_minute)
+        
+        #開始プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation_start_h')).select_by_value(start_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation_start_m')).select_by_value(start_minute)
+        #終了プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation_end_h')).select_by_value(end_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation_end_m')).select_by_value(end_minute)
+
+        #実開始プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(10) > div > div:nth-child(2) > select')).select_by_value(start_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(10) > div > div:nth-child(3) > select')).select_by_value(start_minute)
+
+        #実終了プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(12) > div > div:nth-child(1) > select')).select_by_value(end_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(12) > div > div:nth-child(2) > select')).select_by_value(end_minute)
+
+        #名称
+        driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(18) > input[type=text]').send_keys('バドミントン練習')
+
+        #人数(塾内)
+        driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(20) > div > input:nth-child(1)').clear()
+        driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(20) > div > input:nth-child(1)').send_keys('15')
+
+        #e-mail
+        driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(30) > div > input[type=text]:nth-child(1)').send_keys('t20651sh@sfc.keio.ac.jp')
+
+        #連絡先
+        driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(32) > input[type=text]').send_keys('08014671953')
+
+        #登録する
+        driver.find_element_by_css_selector('#reservation-form > button').click()
+
+        #画面遷移
+
+        #ダイアログ (最終)
+        while not driver.find_element_by_css_selector('body > div:nth-child(20)').is_displayed():
+            print("waiting on display")
+        driver.find_element_by_css_selector('body > div:nth-child(20) > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(1)').click()
+
+        #画面遷移
+        time.sleep(2)
+
+        #完了確認
+        driver.find_element_by_css_selector('body > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog-buttons.ui-draggable.ui-resizable > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button').click()
+
 
         #時間選択 アリーナ奥
         driver.find_element_by_css_selector('#main_content > div.container > div.container_body.noscroll > div.fix_tbl_area.time_table.found-reservable > div.fix_bottom_right > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > div.time_cell.relative > label:nth-child(61)').click()
-        enter(driver, start_hour, start_minute, end_hour, end_minute)
+        #開始プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation_start_h')).select_by_value(start_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation_start_m')).select_by_value(start_minute)
+        #終了プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation_end_h')).select_by_value(end_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation_end_m')).select_by_value(end_minute)
+
+        #実開始プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(10) > div > div:nth-child(2) > select')).select_by_value(start_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(10) > div > div:nth-child(3) > select')).select_by_value(start_minute)
+
+        #実終了プルダウン
+        #時間
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(12) > div > div:nth-child(1) > select')).select_by_value(end_hour)
+        #分
+        Select(driver.find_element_by_css_selector('#reservation-form > dl > dd:nth-child(12) > div > div:nth-child(2) > select')).select_by_value(end_minute)
+
+        #登録する
+        driver.find_element_by_css_selector('#reservation-form > button').click()
+
+        #画面遷移
+
+        #ダイアログ (最終)
+        while not driver.find_element_by_css_selector('body > div:nth-child(20)').is_displayed():
+            print("waiting on display")
+        driver.find_element_by_css_selector('body > div:nth-child(20) > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(1)').click()
+
+        #画面遷移
+        time.sleep(2)
+
+        #完了確認
+        driver.find_element_by_css_selector('body > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog-buttons.ui-draggable.ui-resizable > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button').click()
+
 
     except Exception as e:
         error = str(traceback.format_exc()) +"\ " + str(e)
         if len(error) > 950:
             errors = [error[i: i+950] for i in range(0, len(error), 950)]
-        for j in range(len(errors)):
-            send_line(errors[j])
+            for j in range(len(errors)):
+                send_line(errors[j])
     else:
         message = day + " " + start_hour + ":" + start_minute + "〜" + end_hour + ":" + end_minute + "予約完了"
         send_line(message)
@@ -248,6 +243,50 @@ def reserve(event):
     #main_content > div.container > div.container_body.noscroll > div.fix_tbl_area.time_table.found-reservable > div.fix_bottom_right > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > div.time_cell.relative > label:nth-child(4)
     #main_content > div.container > div.container_body.noscroll > div.fix_tbl_area.time_table.found-reservable > div.fix_bottom_right > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > div.time_cell.relative > label:nth-child(3)時間
 
+
+
+def calender():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    min = (datetime.datetime.utcnow() + datetime.timedelta(days=14)).isoformat() + 'Z' # 'Z' indicates UTC time
+    max = (datetime.datetime.utcnow() + datetime.timedelta(days=15)).isoformat() + 'Z'
+    print('Getting the 2weeks later event')
+    print(min)
+    print(max)
+    events_result = service.events().list(calendarId='3442e499hjv4j581l1c68n4v2g@group.calendar.google.com', timeMin=min,
+                                        timeMax=max,maxResults=1, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        reserve(event)
+        
 
         #ライン送るで
 def send_line(message):
