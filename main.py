2c709c10453032d7284aba09928b0fb8af792c0a
hamasyo <51374934+hamasyo222@users.noreply.github.com>
2022-10-27 12:02:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ def calender(event, context):
 
     # Call the Calendar API
     #予約の判別
-    min = (datetime.datetime.utcnow() + datetime.timedelta(days=14)).isoformat() + 'Z' # 'Z' indicates UTC time
+    min = (datetime.datetime.utcnow() + datetime.timedelta(days=13)).isoformat() + 'Z' # 'Z' indicates UTC time
     max = (datetime.datetime.utcnow() + datetime.timedelta(days=15)).isoformat() + 'Z'
 
     print('Getting the 2weeks later event')
